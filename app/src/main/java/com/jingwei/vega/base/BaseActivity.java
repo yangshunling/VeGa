86f7fe79e9be0c45f0f3bfd47233b046c8ef4d00
RyanSummer <15988161520@139.com>
2018-12-02 17:49:49 +0800

    BaseActivity添加点击空白区域自动隐藏软键盘

--- app/src/main/java/com/jingwei/vega/base/BaseActivity.java
+++ app/src/main/java/com/jingwei/vega/base/BaseActivity.java
@@ -1,10 +1,14 @@
 package com.jingwei.vega.base;
 
 import android.Manifest;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -60,6 +64,9 @@ public abstract class BaseActivity extends AppCompatActivity implements EasyPerm
                 .init();
     }
 
+    /**
+     * 沉浸式图片
+     */
     public void setTransparent() {
         ImmersionBar.with(this).fitsSystemWindows(false).transparentBar().init();
     }
@@ -199,4 +206,53 @@ public abstract class BaseActivity extends AppCompatActivity implements EasyPerm
         ImmersionBar.with(this).destroy();
     }
 
+    /**
+     * 点击非输入框的空白区域自动隐藏软键盘
+     * @param ev
+     * @return
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (isShouldHideInput(v, ev)) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+            return super.dispatchTouchEvent(ev);
+        }
+        // 必不可少，否则所有的组件都不会有TouchEvent了
+        if (getWindow().superDispatchTouchEvent(ev)) {
+            return true;
+        }
+        return onTouchEvent(ev);
+    }
+
+    /**
+     * 点击非输入框的空白区域自动隐藏软键盘
+     * @param v
+     * @param event
+     * @return
+     */
+    public boolean isShouldHideInput(View v, MotionEvent event) {
+        if (v != null && (v instanceof EditText)) {
+            int[] leftTop = { 0, 0 };
+            //获取输入框当前的location位置
+            v.getLocationInWindow(leftTop);
+            int left = leftTop[0];
+            int top = leftTop[1];
+            int bottom = top + v.getHeight();
+            int right = left + v.getWidth();
+            if (event.getX() > left && event.getX() < right
+                    && event.getY() > top && event.getY() < bottom) {
+                // 点击的是输入框区域，保留点击EditText的事件
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
 }
