ecf504ed576d16ec4c974691ff84763742a8310b
yangshunling <yang_shunling@163.com>
2018-12-04 13:28:53 +0800

    完成轮播图效果-待优化

--- app/src/main/java/com/jingwei/vega/fragment/HomeFargment.java
+++ app/src/main/java/com/jingwei/vega/fragment/HomeFargment.java
@@ -1,17 +1,20 @@
 package com.jingwei.vega.fragment;
 
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 
+import com.azoft.carousellayoutmanager.CarouselLayoutManager;
+import com.azoft.carousellayoutmanager.CenterScrollListener;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
 import com.jingwei.vega.R;
 import com.jingwei.vega.adapter.HomeListAdapter;
 import com.jingwei.vega.base.BaseFragment;
 import com.jingwei.vega.moudle.bean.HomeBean;
+import com.jingwei.vega.utils.GlideUtil;
 import com.jingwei.vega.utils.ListViewUtil;
-import com.jingwei.vega.view.cardgallery.CardGalleryAdapter;
-import com.jingwei.vega.view.cardgallery.CardScaleHelper;
-import com.jingwei.vega.view.cardgallery.SpeedRecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,17 +23,18 @@ import butterknife.BindView;
 
 public class HomeFargment extends BaseFragment {
 
-    @BindView(R.id.recyclerView)
-    SpeedRecyclerView mRecyclerView;
     @BindView(R.id.home_list)
     ListView mHomeList;
+    @BindView(R.id.rl_banner)
+    RecyclerView mRlBanner;
 
+    private List<String> mBannerList = new ArrayList<>();
+    private BannerAdapter mBannerAdapter;
 
-    private List<String> mStringList = new ArrayList<>();
-    private CardScaleHelper mCardScaleHelper = null;
-
-    private HomeListAdapter mMyAdapter;
     private List<HomeBean> mBeanList = new ArrayList<>();
+    private HomeListAdapter mListAdapter;
+
+    private CarouselLayoutManager layoutManager;
 
     @Override
     public int getContentView() {
@@ -44,10 +48,16 @@ public class HomeFargment extends BaseFragment {
     @Override
     public void initData() {
         //轮播图测试数据
-        mStringList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1544256191&di=63fa160f4e838ddd1a46c9033f464af9&imgtype=jpg&er=1&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0136db56a8337a32f875520fcda8ec.jpg");
-        mStringList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1543661471451&di=71e9833a2611a25613c44eb954402e3f&imgtype=0&src=http%3A%2F%2F03img.mopimg.cn%2Fmobile%2F20180109%2F20180109170328_dd813f33a2c0ffa72ac02326fe24dbc9_9.jpeg");
-        mStringList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1544256262&di=d8219c847bf9fb14faef076a67ff093c&imgtype=jpg&er=1&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fq_70%2Cc_zoom%2Cw_640%2Fimages%2F20180705%2F4251e5bdadf8438995093c1009dbd3b9.jpeg");
-        initRecyclerView();
+        mBannerList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1544256191&di=63fa160f4e838ddd1a46c9033f464af9&imgtype=jpg&er=1&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0136db56a8337a32f875520fcda8ec.jpg");
+        mBannerList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1543661471451&di=71e9833a2611a25613c44eb954402e3f&imgtype=0&src=http%3A%2F%2F03img.mopimg.cn%2Fmobile%2F20180109%2F20180109170328_dd813f33a2c0ffa72ac02326fe24dbc9_9.jpeg");
+        mBannerList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1544256262&di=d8219c847bf9fb14faef076a67ff093c&imgtype=jpg&er=1&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fq_70%2Cc_zoom%2Cw_640%2Fimages%2F20180705%2F4251e5bdadf8438995093c1009dbd3b9.jpeg");
+        mBannerList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1544256191&di=63fa160f4e838ddd1a46c9033f464af9&imgtype=jpg&er=1&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0136db56a8337a32f875520fcda8ec.jpg");
+        mBannerList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1543661471451&di=71e9833a2611a25613c44eb954402e3f&imgtype=0&src=http%3A%2F%2F03img.mopimg.cn%2Fmobile%2F20180109%2F20180109170328_dd813f33a2c0ffa72ac02326fe24dbc9_9.jpeg");
+        mBannerList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1544256262&di=d8219c847bf9fb14faef076a67ff093c&imgtype=jpg&er=1&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fq_70%2Cc_zoom%2Cw_640%2Fimages%2F20180705%2F4251e5bdadf8438995093c1009dbd3b9.jpeg");
+        mBannerList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1544256191&di=63fa160f4e838ddd1a46c9033f464af9&imgtype=jpg&er=1&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0136db56a8337a32f875520fcda8ec.jpg");
+        mBannerList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1543661471451&di=71e9833a2611a25613c44eb954402e3f&imgtype=0&src=http%3A%2F%2F03img.mopimg.cn%2Fmobile%2F20180109%2F20180109170328_dd813f33a2c0ffa72ac02326fe24dbc9_9.jpeg");
+        mBannerList.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1544256262&di=d8219c847bf9fb14faef076a67ff093c&imgtype=jpg&er=1&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fq_70%2Cc_zoom%2Cw_640%2Fimages%2F20180705%2F4251e5bdadf8438995093c1009dbd3b9.jpeg");
+        initBanner();
         //列表测试数据
         for (int i = 0; i < 3; i++) {
             HomeBean homeBean = new HomeBean();
@@ -67,20 +77,20 @@ public class HomeFargment extends BaseFragment {
 
     private void initListview() {
         //列表
-        mMyAdapter = new HomeListAdapter(getActivity(), mBeanList);
-        mHomeList.setAdapter(mMyAdapter);
+        mListAdapter = new HomeListAdapter(getActivity(), mBeanList);
+        mHomeList.setAdapter(mListAdapter);
         ListViewUtil.setListViewHeightBasedOnChildren(mHomeList);
     }
 
-    private void initRecyclerView() {
-        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
-        mRecyclerView.setLayoutManager(linearLayoutManager);
-        mRecyclerView.setAdapter(new CardGalleryAdapter(getActivity(), mStringList));
-        //mRecyclerView绑定scale效果
-        mCardScaleHelper = new CardScaleHelper();
-        mCardScaleHelper.setCurrentItemPos(2);
-        mCardScaleHelper.attachToRecyclerView(mRecyclerView);
-
+    private void initBanner() {
+        layoutManager = new
+                CarouselLayoutManager(CarouselLayoutManager.HORIZONTAL);
+        layoutManager.setMaxVisibleItems(1);
+        mBannerAdapter = new BannerAdapter(R.layout.item_banner, mBannerList);
+        mRlBanner.setLayoutManager(layoutManager);
+        mRlBanner.setHasFixedSize(false);
+        mRlBanner.setAdapter(mBannerAdapter);
+        mRlBanner.addOnScrollListener(new CenterScrollListener());
     }
 
     @Override
@@ -90,5 +100,17 @@ public class HomeFargment extends BaseFragment {
     @Override
     public void onClick(View v) {
 
+    }
+
+    public class BannerAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
+
+        public BannerAdapter(int layoutResId, List data) {
+            super(layoutResId, data);
+        }
+
+        @Override
+        protected void convert(BaseViewHolder helper, String item) {
+            GlideUtil.setImage(getActivity(), item, (ImageView) helper.getView(R.id.iv_item_banner));
+        }
     }
 }
